883f19cdb63c5eb79ba490a825afc9fbc102b24b
EasyWin <Nicolaecristian100@yahoo.com>
2026-03-09 20:11:31 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,20 +2,23 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-apiKey: "AIzaSyCSapx5d9v_fxpPBzuOfnH66lvQoQ8H-6E",
-authDomain: "taxi-kristian.firebaseapp.com",
-projectId: "taxi-kristian",
-messagingSenderId: "872925880546",
-appId: "1:872925880546:web:12da74898c90a0155b7ebb"
+  apiKey: "AIzaSyCSapx5d9v_fxpPBzuOfnH66lvQoQ8H-6E",
+  authDomain: "taxi-kristian.firebaseapp.com",
+  projectId: "taxi-kristian",
+  messagingSenderId: "872925880546",
+  appId: "1:872925880546:web:12da74898c90a0155b7ebb"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload){
+messaging.onBackgroundMessage(function(payload) {
 
-self.registration.showNotification("🚕 Nová jízda", {
-body: "Máte novou objednávku",
-icon: "/icon.png"
-});
+  const notificationTitle = "🚕 Nová jízda";
+  const notificationOptions = {
+    body: "Máte novou objednávku",
+    icon: "/icon.png"
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 
 });
