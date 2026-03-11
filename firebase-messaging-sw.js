ee4e8bf71ade3a4c213e4d772997723282c05d25
EasyWin <Nicolaecristian100@yahoo.com>
2026-03-11 12:09:38 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-apiKey: "AIzaSyCsapx5d9v_fxpPBzu0fnH66lv0o08H-6E",
+apiKey: "AIzaSyCSapx5d9v_fxpPBzuOfnH66lvQoQ8H-6E",
 authDomain: "taxi-kristian.firebaseapp.com",
 projectId: "taxi-kristian",
 messagingSenderId: "872925880546",
@@ -14,10 +14,22 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage(function(payload){
 
 self.registration.showNotification("🚕 Cursă nouă",{
-body: payload.data.body,
-icon: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
+body: payload.data?.body || "Ai primit o cursă nouă",
+icon: "/Driver-taxi/icon.png",
+
 vibrate: [300,100,300,100,300],
-requireInteraction: true
+
+requireInteraction: true,
+
+tag: "new-ride",
+
+actions: [
+{
+action: "open",
+title: "Deschide aplicația"
+}
+]
+
 });
 
 });
